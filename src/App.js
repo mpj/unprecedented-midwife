0f1cf707fab1569e7f64bd219fc32db9d7216e36
David Nimmo <dnimmo@gmail.com>
2017-03-20 00:34:01 +0000

    Why not add StandardJS as well? You wouldn't want a semi-colon sneaking in to your Hellow World app. :O

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
+      <div className='App'>
         {this.props.greeting}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
